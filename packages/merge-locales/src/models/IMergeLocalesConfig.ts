53c4a81ef5c9709168b10b71caeeff7d8537fea9
Martyn Janes <martyn@obany.com>
2024-06-03 15:09:02 +0200

    Add merge-locales tool

---
+++ packages/merge-locales/src/models/IMergeLocalesConfig.ts
@@ -0,0 +1,29 @@
+// Copyright 2024 IOTA Stiftung.
+// SPDX-License-Identifier: Apache-2.0.
+
+import type { ILocale } from "@gtsc/core";
+
+/**
+ * Configuration for the CLI.
+ */
+export interface IMergeLocalesConfig {
+	/**
+	 * The languages to include while merging, if none are supplied only English will be included.
+	 */
+	locales?: ILocale[];
+
+	/**
+	 * Additional packages to add locales for, which are not part of the dependencies.
+	 */
+	includePackages?: string[];
+
+	/**
+	 * Packages to exclude from the locales.
+	 */
+	excludePackages?: string[];
+
+	/**
+	 * Output directory for the merged locales.
+	 */
+	outputDirectory?: string;
+}
