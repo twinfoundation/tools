7d116398a439f37ffbf4f20d141333941e4f1aba
Martyn Janes <martyn@obany.com>
2024-10-09 11:23:18 +0200

    Update test config

--- packages/nameof-transformer/vitest.config.ts
+++ packages/nameof-transformer/vitest.config.ts
@@ -1,14 +1,17 @@
 // Copyright 2024 IOTA Stiftung.
 // SPDX-License-Identifier: Apache-2.0.
 import typescript from "rollup-plugin-typescript2";
-import tspc from "ts-patch/compiler";
+// import tspc from "ts-patch/compiler";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	esbuild: false,
 	plugins: [
 		typescript({
-			tsconfig: "./tests/tsconfig.json"
+			tsconfig: "./tests/tsconfig.json",
+			// Disabled in this repo as this is the transformer
+			// typescript: tspc,
+			clean: true
 		})
 	],
 	test: {
